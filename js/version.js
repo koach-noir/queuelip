f160d299e41175d66e1a79f768ea5231827bceca
koach-noir <noe.k@ymail.ne.jp>
2025-05-23 21:08:20 +0900

    [release] バージョン0.1.128 - メインウィンドウ自動クローズ機能追加
    - miniウィンドウ開時にメインウィンドウを自動クローズ
    - miniウィンドウクローズ時にアプリケーション終了
    - ウィンドウライフサイクル管理の最適化
    - ユーザビリティ向上（複数ウィンドウ混在状態の解消）
    - リソース管理の最適化

--- js/version.js
+++ js/version.js
@@ -1,6 +1,6 @@
 // バージョン情報と最終更新日時の管理
-export const APP_VERSION = '0.1.127';
-export const LAST_UPDATE = '2025年05月23日 20:55:00';
+export const APP_VERSION = '0.1.128';
+export const LAST_UPDATE = '2025年05月23日 21:08:00';
 
 // バージョン情報を画面に表示する
 export function displayVersionInfo() {
