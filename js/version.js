59de6634e0422dd8ee0cca4273d58ece8d5cb355
Yutaka Koach <yutakakoach@ymbair.local>
2025-06-07 14:23:13 +0900

    [release] バージョン0.1.143
    debug: アーティファクト構造デバッグとファイル指定修正
    - リリースジョブでアーティファクト内容を詳細表示
    - ファイル指定をartifacts/**/*に変更してテスト

--- js/version.js
+++ js/version.js
@@ -1,6 +1,6 @@
 // バージョン情報と最終更新日時の管理
-export const APP_VERSION = '0.1.142';
-export const LAST_UPDATE = '2025年06月07日 14:15:00';
+export const APP_VERSION = '0.1.143';
+export const LAST_UPDATE = '2025年06月07日 14:25:00';
 
 // バージョン情報を画面に表示する
 export function displayVersionInfo() {
